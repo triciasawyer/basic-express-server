2b57b51810e4d707edd82c1a047eb13bc8c1e8e8
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-16 20:46:00 -0500

    fill out error 500

---
+++ src/error-handlers/500.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+module.exports = (error, req, res, next) => {
+  const errorMessage = typeof(error) === 'string' ? error : error.message;
+  res.status(500).send ({
+
+    error: 500,
+    route: req.path,
+    query: req.queury,
+    path: req.params,
+    body: req.body,
+    message: `Server Error: ${errorMessage}`,
+  });
+};
