e06d7a573edd99fe31b5b8a9dcee21eccadfda0e
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-16 20:19:31 -0500

    build out server

---
+++ src/server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require ('express');
+const cors = require('cors');
+
+
+
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res, next) => {
+  res.status(200).send('Proof of life');
+});
+
+
